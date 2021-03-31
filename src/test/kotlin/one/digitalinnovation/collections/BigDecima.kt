6de171d9d533fa55bc65594faddbd93e690e3772
silver21br <silverja@gmail.com>
2021-03-30 21:25:40 -0300

    Commit final do curso 2

--- src/test/kotlin/one/digitalinnovation/collections/BigDecima.kt
+++ src/test/kotlin/one/digitalinnovation/collections/BigDecima.kt
@@ -7,6 +7,7 @@ fun Array<BigDecimal>.somatoria() = this.reduce {
         acc, valor -> acc + valor
 }
 
+
 fun Array<BigDecimal>.media() =
     if(this.isEmpty()) BigDecimal.ZERO
     else this.somatoria() / this.size.toBigDecimal()
