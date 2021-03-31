b21b03e87898ca1f66100a10b52f83fad809cab5
silver21br <silverja@gmail.com>
2021-03-30 21:22:14 -0300

    Commit final do curso

---
+++ src/test/kotlin/one/digitalinnovation/collections/TesteFuncoesEstendidas.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "1500".toBigDecimal(),
+        "4000".toBigDecimal(),
+    )
+
+    println("----------")
+
+    salarios.forEach { salario ->
+        println(salario)
+    }
+    println("----------")
+    println(salarios.somatoria())
+    println("----------")
+    println(salarios.media())
+}
